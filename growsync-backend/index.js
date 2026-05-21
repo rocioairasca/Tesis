6ee650af1a309f3a783d8a80ed284331f7ed8b49
Rocio <rocioairasca@hotmail.com>
2026-05-21 10:40:09 -0300

    çambios back para deploy render

--- growsync-backend/index.js
+++ growsync-backend/index.js
@@ -24,10 +24,12 @@ const supabase = require('./db/supabaseClient');
 // Middleware
 // ---------------------------------------------------
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? [process.env.FRONTEND_URL]
-    : ["http://localhost:3000"],
-  credentials: true
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000",
+    "http://localhost:5173",
+  ],
+  credentials: true,
 }));
 
 app.use(express.json());
